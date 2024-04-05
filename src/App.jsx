455116d0cd26fb291dc5b12a7b1efe8a7a1a94e2
IfeayiPrime <ifeanyi20162016@outlook.com>
2024-04-05 23:56:17 +0100

    added modal and styled form

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
-import './App.css';
+import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
   const [role, setRole] = useState('dev');
+  const [employees, setEmployees] = useState(
+    [
+      {name: "Caleb", role:"intern", img:"/images/pexels-andrea-piacquadio-762080.jpg"},
+      {name: "Sal", role:"Manager", img:"/images/pexels-andrea-piacquadio-837358.jpg"},
+      {name: "John", role:"Director of Eng.", img:"/images/pexels-andrea-piacquadio-845457.jpg"},
+      {name: "Melanie", role:"Software Engineer", img:"/images/pexels-andrea-piacquadio-762080.jpg"},
+      {name: "Corey", role:"The Devops guy", img:"/images/pexels-andrea-piacquadio-837358.jpg"},
+      {name: "Jake", role:"Senior Intern", img:"/images/pexels-andrea-piacquadio-845457.jpg"}
+    ]
+  )
   const showEmployee = true;
   return (
     <div className="App">
-      {console.log('inside the return')}
       {showEmployee ? (
       <>
       <input type='text' onChange={(e) => {
         console.log(e.target.value);
         setRole(e.target.value);
       }} />
-          <Employee name="Caleb" role="intern"/>
-          <Employee name="Abby" role={role}/>
-          <Employee name="John"/>
+      <div className='flex flex-wrap justify-center'>
+          {employees.map((employee) => {
+            return (
+            <Employee 
+              key={uuidv4()}
+              name={employee.name} 
+              role={employee.role} 
+              img={employee.img} 
+            />
+            )
+          })}
+      </div>
       </>
       ) : (
         <p>You cannot see the employees</p>
