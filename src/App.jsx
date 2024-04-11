03305ba29908ce69e0810a84959ef7e164ca0a20
IfeayiPrime <ifeanyi20162016@outlook.com>
2024-04-11 19:50:36 +0100

    added an 'AddEmployee' component, and passed component 'EditEmployee' from parent

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,113 @@
-import './index.css';
-import Employee from './components/Employee';
-import { useState } from 'react';
-import {v4 as uuidv4} from 'uuid';
+import "./index.css";
+import Employee from "./components/Employee";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import AddEmployee from "./components/AddEmployee";
+import EditEmployee from "./components/EditEmployee";
 
 function App() {
-  const [role, setRole] = useState('dev');
-  const [employees, setEmployees] = useState(
-    [
-      {name: "Caleb", role:"intern", img:"/images/pexels-andrea-piacquadio-762080.jpg"},
-      {name: "Sal", role:"Manager", img:"/images/pexels-andrea-piacquadio-837358.jpg"},
-      {name: "John", role:"Director of Eng.", img:"/images/pexels-andrea-piacquadio-845457.jpg"},
-      {name: "Melanie", role:"Software Engineer", img:"/images/pexels-andrea-piacquadio-762080.jpg"},
-      {name: "Corey", role:"The Devops guy", img:"/images/pexels-andrea-piacquadio-837358.jpg"},
-      {name: "Jake", role:"Senior Intern", img:"/images/pexels-andrea-piacquadio-845457.jpg"}
-    ]
-  )
+  const [role, setRole] = useState("dev");
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      name: "Caleb",
+      role: "intern",
+      img: "/images/pexels-andrea-piacquadio-762080.jpg",
+    },
+    {
+      id: 2,
+      name: "Sal",
+      role: "Manager",
+      img: "/images/pexels-andrea-piacquadio-837358.jpg",
+    },
+    {
+      id: 3,
+      name: "John",
+      role: "Director of Eng.",
+      img: "/images/pexels-andrea-piacquadio-845457.jpg",
+    },
+    {
+      id: 4,
+      name: "Melanie",
+      role: "Software Engineer",
+      img: "/images/pexels-andrea-piacquadio-762080.jpg",
+    },
+    {
+      id: 5,
+      name: "Corey",
+      role: "The Devops guy",
+      img: "/images/pexels-andrea-piacquadio-837358.jpg",
+    },
+    {
+      id: 6,
+      name: "Jake",
+      role: "Senior Intern",
+      img: "/images/pexels-andrea-piacquadio-845457.jpg",
+    },
+  ]);
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id == employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+      return employee;
+    });
+    console.log(updatedEmployees);
+    setEmployees(updatedEmployees);
+  }
+  function newEmployee(name, role, img) {
+    let newEmployee = {
+      id: uuidv4(),
+      name,
+      role,
+      img,
+    };
+    setEmployees([...employees, newEmployee]);
+  }
+
   const showEmployee = true;
   return (
     <div className="App">
       {showEmployee ? (
-      <>
-      <input type='text' onChange={(e) => {
-        console.log(e.target.value);
-        setRole(e.target.value);
-      }} />
-      <div className='flex flex-wrap justify-center'>
-          {employees.map((employee) => {
-            return (
-            <Employee 
-              key={uuidv4()}
-              name={employee.name} 
-              role={employee.role} 
-              img={employee.img} 
-            />
-            )
-          })}
-      </div>
-      </>
+        <>
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              const editEmployee = (
+                <EditEmployee
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  updateEmployee={updateEmployee}
+                />
+              );
+              return (
+                <Employee
+                  key={employee.id}
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                  editEmployee={editEmployee}
+                />
+              );
+            })}
+          </div>
+          <AddEmployee newEmployee={newEmployee} />
+        </>
       ) : (
         <p>You cannot see the employees</p>
-      )
-      };
+      )}
+      ;
     </div>
   );
 }
 
-export default App
+export default App;
